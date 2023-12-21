ce729d7d954adbfc240545701b23ed7962efe116
AlessandroGCatino <catino.alessandrog@gmail.com>
2023-12-21 15:26:02 +0100

    esercizio base completato

---
+++ main.js
@@ -0,0 +1,7 @@
+let nomeUtente = prompt("Inserisci il tuo nome:", "nome");
+let cognomeUtente = prompt("Inserisci il tuo cognome:", "cognome");
+let coloreUtente = prompt("Inserisci il tuo colore preferito:", "colorePrefe");
+
+document.writeln(`<h1> ${nomeUtente + cognomeUtente + coloreUtente} </h1>`);
+
+console.log(nomeUtente, cognomeUtente, coloreUtente);
