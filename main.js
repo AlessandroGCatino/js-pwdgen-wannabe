5b1e3f6192c089137ee0f1816287f2cb3835b719
AlessandroGCatino <catino.alessandrog@gmail.com>
2023-12-21 16:13:28 +0100

    bonus: inserito form per l'inserimento dei dati

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
+/*
 let nomeUtente = prompt("Inserisci il tuo nome:", "nome");
 let cognomeUtente = prompt("Inserisci il tuo cognome:", "cognome");
 let coloreUtente = prompt("Inserisci il tuo colore preferito:", "colorePrefe");
-
 document.writeln(`<h1> ${nomeUtente + cognomeUtente + coloreUtente} </h1>`);
 
 console.log(nomeUtente, cognomeUtente, coloreUtente);
+*/
+
+function creaPassword(){
+    let nomeUtente = document.getElementById("nome").value;
+    let cognomeUtente = document.getElementById("cognome").value;
+    let coloreUtente = document.getElementById("colore").value;
+    document.getElementById("password").innerHTML=nomeUtente + cognomeUtente + coloreUtente;
+    console.log(nomeUtente, cognomeUtente, coloreUtente);
+}
